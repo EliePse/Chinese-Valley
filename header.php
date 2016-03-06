9368c8e486470710271a943b786ad06b393d8650
EliePse <eliepse.meignan@gmail.com>
2016-03-06 11:05:18 +0100

    Done the structure

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head <?php language_attributes(); ?>>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title><?php the_title(); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+		<?php wp_head(); ?>
+	</head>
+	<body>
