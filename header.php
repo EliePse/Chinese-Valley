00cf6371eb9650ad88dbad23b0051eb0ee43b90e
Eliepse <eliepse.meignan@gmail.com>
2016-03-09 15:37:51 +0100

    Correct the print of the title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 	<head <?php language_attributes(); ?>>
 		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php the_title(); ?></title>
+		<title><?php echo get_bloginfo( 'name' ); ?></title>
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
 		<?php wp_head(); ?>
 	</head>
